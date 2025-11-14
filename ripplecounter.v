3d884d9794a16f97bee331d8713a35ebc2683786
Broden Benson <brodenbenson@gmail.com>
2025-11-14 10:53:41 -0700

    Add reset input to ripple counter module

--- ripplecounter.v
+++ ripplecounter.v
@@ -1,27 +1,28 @@
 module ripplecounter(
-    input clk,
+    input clk, Rst,
     output wire stage1, stage2, stage3
-);
-    reg t; 
-    initial begin
-        t = 1'b1;
-    end
-    
-        
+    ); 
     tflipflop s1 (
-        .T(t),
-        .Clk(clk),
-        .Q(stage1)
+       .Rst(Rst),
+       .T(1'b1),
+       .Clk(clk),
+       .Q(stage1)
     );
     
     tflipflop s2 (
+    .Rst(Rst),
+        .T(1'b1),
         .Clk(stage1),
         .Q(stage2)
     );
     
     tflipflop s3 (
+    .Rst(Rst),
+        .T(1'b1),
         .Clk(stage2),
         .Q(stage3)
     );
+    
+    
     
 endmodule
