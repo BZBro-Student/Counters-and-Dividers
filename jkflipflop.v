81e61cdaf5262d1380bd0c9cfea1473874abcbbf
Broden Benson <brodenbenson@gmail.com>
2025-11-14 10:52:53 -0700

    Add reset input to jkflipflop module

--- jkflipflop.v
+++ jkflipflop.v
@@ -1,11 +1,12 @@
 module jkflipflop(
-    input J, K, Clk,
+    input J, K, Clk,Rst,
     output Q, notQ
     );
     wire din = (~K & Q) | (J & ~Q);
         
         
         dflipflop d(
+        .Rst(Rst),
         .Din(din),
         .Clk(Clk),
         .Q(Q),
